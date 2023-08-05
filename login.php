b2b2c10ef0ee4f873d194c700669b96b67f9d682
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-05 20:51:51 +1200

    testing

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ $user->password = 'admin';
 $dbuser = authenticateUser($user);
 
 setcookie("token", $dbuser['token'], $dbuser['expiry']);
-echo 'Created token & cookie ' . $dbuser['token'] . "\n\n" . ' Cookie value: ' . $_COOKIE['token'];
+header("Location: https://paradisecoffee.cafe/admin/home.php");
+exit();
 
 ?>
