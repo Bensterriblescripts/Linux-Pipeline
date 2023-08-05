6dbf9dbba8652ceaf9fca70ab81658ef4b79a0f8
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-05 19:11:33 +1200

    maybe not redirecting?

--- login.php
+++ login.php
@@ -11,6 +11,11 @@ $dbuser = authenticateUser($user);
 setcookie("token", $dbuser['token'], $dbuser['expiry']);
 echo 'Created token & cookie ' . $dbuser['token'] . "\n\n" . 'Cookie value: ' . $_COOKIE['token'];
 
+header("Location: https://paradisecoffee.cafe/login.php");
+exit();
+
+
+
 ?>
 <html>
     <style>
