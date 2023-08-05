689ba7016db80d92b7a5a8daa81a13d1029c0438
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-05 19:08:51 +1200

    more cookie testing

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $user->password = 'admin';
 $dbuser = authenticateUser($user);
 
 setcookie("token", $dbuser['token'], $dbuser['expiry']);
-echo 'Created token & cookie' . $dbuser['token'];
+echo 'Created token & cookie ' . $dbuser['token'] . "\n\n" . 'Cookie value: ' . $_COOKIE['token'];
 
 ?>
 <html>
