3a915b5bcd8ca6ef990c35da341c73719c42b72e
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-05 13:18:49 +1200

    fixed array

--- components/db.php
+++ components/db.php
@@ -43,7 +43,9 @@ function authenticateUser($user) {
     $ranstr = rand();
     $token = hash("sha256", $ranstr);
 
-    $query = "INSERT INTO auth_token (username, token, timeadded, expiry) VALUES ('$dbuser['username']', '$token', $timeadded, $expiry)";
+    $username = $dbuser['username'];
+
+    $query = "INSERT INTO auth_token (username, token, timeadded, expiry) VALUES ('$username', '$token', $timeadded, $expiry)";
     $result = pg_query($db, $query);
     if (!$result) {
         echo 'Error authenticating the user';
