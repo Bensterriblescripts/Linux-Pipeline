42b21e0f1012cb621865ed9456524cf4d04cf042
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-13 17:24:35 +1200

    still fixin

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (isset($_COOKIE['token'])) {
             background-color: #f0f0f0;
         }
         #core {
-            background-image: url(../assets/photos/PC_Flower_Pink.png);
+            background-image: url(../assets/photos/PC_Flower_Pink_MEDIUM.png);
             margin-top: 9rem;
             margin-left: auto;
             margin-right: auto;
