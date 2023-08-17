6fecc3a0643584b23eeb4cd48993b0dba4e174ac
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-17 21:24:55 +1200

    Update core.py

--- core.py
+++ core.py
@@ -3,42 +3,46 @@ import os
 repositories = [
     "Template-Projects", "Website-CoffeeOrders", "Linux-Pipeline"
 ]
-baseurl = "https://github.com/BensterribleScripts/"
+baseurl = "https://github.com/Bensterriblescripts/"
 
-dir = "/home/ben/Repo"
+# Move to Repo Directory
+dir = "/home/ben/Repositories"
+if os.path.exists(dir) == False:
+    os.system("mkdir Repositories")
 os.chdir(dir)
 
-# Setup Git Repo
-def create(repo, url):
+# Setup Git Repository
+def create(repo, url, dir):
     os.system("git init")
-    os.system("git remote add " + repo + " " + url + repo + ".git")
     os.system("git config --global credential.helper store")
-    os.system("git push --set-upstream " + repo + " master")
+    if os.path.exists(dir + "/" + repo) == False: 
+        os.system("git clone " + url + repo + ".git")
 
-def createAll(repositories, url):
+def createAll(repositories, url, dir):
+    os.system("git init")
     os.system("git config --global credential.helper store")
     for repository in repositories:
-        os.system("git init")
-        os.system("git clone " + url + repository + ".git")
-        os.system("git remote add " + repository + " " + url + repository + ".git")
-        os.system("git push --set-upstream " + repository + " master")
+        if os.path.exists(dir + "/" + repository) == False:
+            os.system("git clone " + url + repository + ".git")
 
 # Push
 def push(repo, url, dir):
     try:
-        os.system("git add -A " + dir + "/" + repo)
-        os.system("git push -f " + repo + " origin master")
+        os.system("git add -A")
+        os.system("git push -f --set-upstream " + url + repo + " .git master")
     except:
         create(repo, url)
 
 def pushAll(repositories, url, dir):
+    os.system("git add -A")
+    os.system('git commit -m "Auto-Push"')
     for repository in repositories:
+        os.system("git pull " + repository)
         print("Updating " + repository + ".....")
         try:
-            os.system("git add -A " + dir + "/" + repository)
-            os.system("git push -f " + url + repository + ".git" + " master")
+            os.system("git push --set-upstream " + url + repository + ".git master")
         except: 
             print("Push failed on: " + repository)
+
+
 pushAll(repositories, baseurl, dir)
-
-
