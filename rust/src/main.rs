98503444d7ae20c38020d68011365810794f78f7
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-23 19:57:26 +1200

    'Auto-Push'

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let repositories: [&str; 2] = ["Linux-Pipeline", "CoffeeOrders-Website"];
 
-    println!("1: Push\n2: Pull\n");
+    println!("1: Push\n2: Pull");
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
     if input.trim() == "1" {
@@ -28,19 +28,46 @@ fn push(basedir: &str, repositories: [&str; 2]) {
         let output = Command::new("cmd")
             .args(&["/C", "git add -A"])
             .current_dir(&dir)
-            .output();
+            .output()
+            .expect("Failed to run pull command");
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } 
+        else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            println!("{}", stderr);
+        }
 
         // Commmit message
         let output = Command::new("cmd")
             .args(&["/C", "git commit -m 'Auto-Push'"])
             .current_dir(&dir)
-            .output();
+            .output()
+            .expect("Failed to run pull command");
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } 
+        else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            println!("{}", stderr);
+        }
 
         // Push
         let output = Command::new("cmd")
             .args(&["/C", "git push"])
             .current_dir(&dir)
-            .output();
+            .output()
+            .expect("Failed to run pull command");
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } 
+        else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            println!("{}", stderr);
+        }
     }
 
 }
@@ -53,6 +80,15 @@ fn pull(basedir: &str, repositories: [&str; 2]) {
         let output = Command::new("cmd")
             .args(&["git pull"])
             .current_dir(&dir)
-            .output();
+            .output()
+            .expect("Failed to run pull command");
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } else 
+        {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            println!("{}", stderr);
+        }
     }
 }
