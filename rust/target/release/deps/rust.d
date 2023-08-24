450b2298b39d16f8e3fe26321722a5636f3d68eb
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-25 07:57:56 +1200

    'Auto-Push'

---
+++ rust/target/release/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Local\Repositories\Linux-Pipeline\rust\target\release\deps\rust.exe: src\main.rs
+
+C:\Local\Repositories\Linux-Pipeline\rust\target\release\deps\rust.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/release/rust.d
@@ -0,0 +1,1 @@
+C:\Local\Repositories\Linux-Pipeline\rust\target\release\rust.exe: C:\Local\Repositories\Linux-Pipeline\rust\src\main.rs
